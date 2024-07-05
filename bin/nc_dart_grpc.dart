1c09a668f7a19b5c838b70c4a1dec74f74be7e2c
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-07-05 14:23:39 +0700

    chore: dart fresh project

---
+++ bin/nc_dart_grpc.dart
@@ -0,0 +1,5 @@
+import 'package:nc_dart_grpc/nc_dart_grpc.dart' as nc_dart_grpc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nc_dart_grpc.calculate()}!');
+}
---
+++ test/nc_dart_grpc_test.dart
@@ -0,0 +1,8 @@
+import 'package:nc_dart_grpc/nc_dart_grpc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
