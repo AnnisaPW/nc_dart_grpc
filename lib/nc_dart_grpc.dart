3b459673b9e47e2cb2d414f8da5510df880b01a1
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-07-10 01:11:42 +0700

    chore: export files

--- lib/nc_dart_grpc.dart
+++ lib/nc_dart_grpc.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
-}
+export 'src/db.dart';
+export 'src/generated/album.pb.dart';
+export 'src/generated/album.pbenum.dart';
+export 'src/generated/album.pbjson.dart';
+export 'package:nc_dart_grpc/src/generated/album.pbgrpc.dart';
