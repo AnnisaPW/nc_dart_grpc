5b669b5d4112a751c2206a1d8ec17d5a1fdbd18c
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-07-10 23:37:31 +0700

    feat: specify get data

--- bin/server.dart
+++ bin/server.dart
@@ -4,6 +4,19 @@ import 'package:nc_dart_grpc/nc_dart_grpc.dart';
 class AlbumService extends AlbumServiceBase {
   @override
   Future<AlbumResponse> getAlbums(ServiceCall call, AlbumRequest request) async {
+    if (request.id > 0) {
+      final album = albums
+          .where(
+            (element) => element['id'] == request.id,
+          )
+          .map(
+            (e) => Album.fromJson(
+              '{"1": ${e['id']}, "2": "${e['title']}"}',
+            ),
+          )
+          .toList();
+      return AlbumResponse()..albums.addAll(album);
+    }
     final albumList = albums
         .map(
           (alb) => Album.fromJson(
