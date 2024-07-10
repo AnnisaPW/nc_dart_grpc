5f66ada55c0ace05e91c87bde70bf4bde405c1ad
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-07-10 23:28:46 +0700

    feat: function for read data

--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,26 @@
-import 'package:grpc/src/server/call.dart';
+import 'package:grpc/grpc.dart';
 import 'package:nc_dart_grpc/nc_dart_grpc.dart';
 
 class AlbumService extends AlbumServiceBase {
   @override
-  Future<AlbumResponse> getAlbums(ServiceCall call, AlbumRequest request) {
-    // TODO: implement getAlbums
-    throw UnimplementedError();
+  Future<AlbumResponse> getAlbums(ServiceCall call, AlbumRequest request) async {
+    final albumList = albums
+        .map(
+          (alb) => Album.fromJson(
+            '{"1": ${alb['id']}, "2": "${alb['title']}"}',
+          ),
+        )
+        .toList();
+    return AlbumResponse()..albums.addAll(albumList);
   }
+}
+
+void main() async {
+  final server = Server.create(
+    services: [
+      AlbumService(),
+    ],
+  );
+  await server.serve(port: 5000);
+  print('Server listening on port ${server.port}');
 }
